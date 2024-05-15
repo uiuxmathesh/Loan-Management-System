0939fd80acb8ff4e0cb31ccd4a8c819ece164fc8
uiuxmathesh <matheshpremkumar@gmail.com>
2024-05-15 10:54:33 +0530

    Defined SQL Database Structure: Seed data made

---
+++ Schema.sql
@@ -0,0 +1,33 @@
+IF NOT EXISTS(SELECT * FROM sys.databases WHERE [name]='LoanManager')
+	BEGIN
+		CREATE DATABASE [LoanManager]
+	END
+	GO
+USE	[LoanManager]
+GO
+
+IF OBJECT_ID(N'Customer') IS NULL
+CREATE TABLE [Customer](
+	[CustomerID] int NOT NULL,
+	[Name] varchar(255),
+	[Email] varchar(255),
+	[Phone] varchar(10),
+	[Address] varchar(255),
+	[Credit_score] float,
+	CONSTRAINT Customer_pk PRIMARY KEY ([CustomerID])
+);
+
+
+IF OBJECT_ID(N'Loan') IS NULL
+CREATE TABLE [Loan](
+	[LoanID] int NOT NULL,
+	[CustomerID] int,
+	[principal_amount] float,
+	[interest_rate] float,
+	[loan_term] int,
+	[loan_type] varchar(255) NOT NULL CHECK ([loan_type]IN ('CarLoan','HomeLoan')),
+	[loan_status] varchar(255) NOT NULL CHECK ([loan_status]IN ('Pending','Approved')),
+	CONSTRAINT loan_pk PRIMARY KEY ([CustomerID]),
+	CONSTRAINT customer_loan_fk FOREIGN KEY ([CustomerID]) REFERENCES Customer([CustomerID])
+);
+
---
+++ Seed.sql
@@ -0,0 +1,21 @@
+INSERT INTO Customer (CustomerID, Name, Email, Phone, Address, Credit_score)
+VALUES
+    (1, 'John Doe', 'john.doe@example.com', '1234567890', '123 Main St, Anytown, USA', 750),
+    (2, 'Jane Smith', 'jane.smith@example.com', '9876543210', '456 Elm St, Somewhere, USA', 800),
+    (3, 'Michael Johnson', 'michael.johnson@example.com', '1112223333', '789 Oak St, Nowhere, USA', 700),
+    (4, 'Emily Brown', 'emily.brown@example.com', '4445556666', '321 Maple St, Anytown, USA', 720),
+    (5, 'David Lee', 'david.lee@example.com', '7778889999', '654 Pine St, Elsewhere, USA', 780);
+
+
+INSERT INTO Loan (LoanID, CustomerID, principal_amount, interest_rate, loan_term, loan_type, loan_status)
+VALUES
+    (101, 1, 20000, 5.5, 36, 'CarLoan', 'Pending'),
+    (102, 2, 300000, 4.25, 360, 'HomeLoan', 'Approved'),
+    (103, 3, 15000, 6.0, 24, 'CarLoan', 'Approved'),
+    (104, 4, 250000, 3.75, 240, 'HomeLoan', 'Approved'),
+    (105, 5, 10000, 7.0, 12, 'CarLoan', 'Pending');
+
+
+SELECT * FROM [Customer]
+
+SELECT * FROM [Loan]
