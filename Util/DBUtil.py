de1a0afea7fe7b41a15769421c58fff188b6fa32
uiuxmathesh <matheshpremkumar@gmail.com>
2024-05-15 12:25:32 +0530

    MinorFix: Connection handler is fixed

--- Util/DBUtil.py
+++ Util/DBUtil.py
@@ -8,43 +8,23 @@ class DBUtil:
 
     @staticmethod
     def getDBConn():
-        if DBConnUtil.conn is None:
+        if DBUtil.conn is None:
             connectionString = PropertyUtil.getPropertyString()
             try:
-                DBConnUtil.conn = pyodbc.connect(connectionString)
+                DBUtil.conn = pyodbc.connect(connectionString)
             except ConnectionError as err:
                 print(f"Failed to establish connection: {err}")
         else:
             print("Connection already established")
-        return DBConnUtil.conn
+        return DBUtil.conn
+    
+    @staticmethod
+    def closeConnection():
+        if DBUtil.conn is not None:
+            DBUtil.conn.close()
+            DBUtil.conn = None
+        else:
+            print("No such connection exists")
+        return DBUtil.conn
     
     
-    # conn = None
-
-    # def __init__(self):
-    #     self.connection = self.getConnection()
-    #     self.cursor = self.connection.cursor()
-
-    # def close(self):
-    #     self.closeConnection()
-    
-    # def getConnection(self):
-    #     if self.conn is None:
-    #         connectionString = PropertyUtil.getPropertyString()
-    #         try:
-    #             self.conn = pyodbc.connect(connectionString)
-    #         except ConnectionError as err:
-    #             print(f"Failed to establish connection: {err}")
-    #     else:
-    #         print("Connection already established")
-    #     return self.conn
-
-
-    # def closeConnection(self):
-    #     if self.conn is not None:
-    #         self.conn.close()
-    #         self.conn = None
-        
-    #     else:
-    #         print("No such connection exists")
-    #     return self.conn
