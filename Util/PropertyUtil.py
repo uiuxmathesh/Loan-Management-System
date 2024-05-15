714108c26d9e4f3e152a6e95da5bff3140190818
uiuxmathesh <matheshpremkumar@gmail.com>
2024-05-15 12:06:05 +0530

    Connection handling scripts are written

---
+++ Util/PropertyUtil.py
@@ -0,0 +1,11 @@
+
+
+class PropertyUtil:
+
+    @staticmethod
+    def getPropertytring():
+        SERVER_NAME = "Mathesh-PC"
+        DATABASE_NAME = "LoanManager"
+        TRUSTED_CONNECTION = "Yes"
+        CONNECTION_STRING = f"Driver={{SQL Server}}; Server={SERVER_NAME}; Database={DATABASE_NAME}; Trusted_Connection={TRUSTED_CONNECTION}"
+        return CONNECTION_STRING
